e7ffea142cfea3d60b205953f20839de2781f2f3
dappernerddesigns <verity.gregory@gmail.com>
2022-01-10 13:49:37 +0000

    incorrect use of cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const apiRouter = require('./routers/api.router')
 const cors = require('cors')
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
 const {
